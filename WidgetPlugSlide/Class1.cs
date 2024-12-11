02a6cfbf2e2e7dbfa8bd8643511a66e47e8d4dac
Dyyt587 <67887002+Dyyt587@users.noreply.github.com>
2024-12-11 10:14:39 +0800

    t

--- WidgetPlugSlide/Class1.cs
+++ WidgetPlugSlide/Class1.cs
@@ -1,13 +1,29 @@
-using System;
+﻿using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
-
+using ACOMPlugin.Core;
+using DryIoc;
+using Serilog;
+using ShadowPluginLoader.MetaAttributes;
+using ShadowPluginLoader.WinUI;
+using ShadowPluginLoader.WinUI.Models;
 // To learn more about WinUI, the WinUI project structure,
 // and more about our project templates, see: http://aka.ms/winui-project-info.
 
-namespace WidgetPlugSlide;
-public class Class1
+// 示例的插件主类
+namespace ACOMPlugin.WidgetPluginSlide;
+
+[AutoPluginMeta]
+public partial class WidgetPluginSlide : ACOMPluginBase
 {
+    public override string DisplayName => throw new NotImplementedException();
+
+    public override string Id => throw new NotImplementedException();
+
+    public override string GetEmoji()
+    {
+        throw new NotImplementedException();
+    }
 }
